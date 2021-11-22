8fd07bba860be77f9735f00837615e5f7015194e
Ainun <ayi.jayz13@gmail.com>
2021-11-22 23:20:55 +0800

    added highlight laporan contents

--- components/dashboard/HgLaporan.jsx
+++ components/dashboard/HgLaporan.jsx
@@ -14,10 +14,16 @@ export default function HgLaporan() {
     >
       <div className="text-center h-24  flex-col flex items-center p-4  m-4 rounded-xl">
         <p>
-          Terjadi Peperangan di daerah Gowa. Laporan telah diteruskan ke pihak
-          kepolisisan setempat
+          Terjadi Peperangan di daerah Gowa. Laporan telah diteruskan ke pihak kepolisian setempat
         </p>
         <p>11/21/2021</p>
+        <p>3:26</p>
+      </div>
+      <div className="text-center h-24  flex-col flex items-center">
+        <p>
+          Terjadi kebakaran yang melahap 3 rumah di Sudiang. Lapora telah diteruskan kepada Damkar dan akan tiba dalam waktu 10 menit
+        </p>
+        <p>10/29/2021</p>
         <p>3:26</p>
       </div>
       {/* <div className="text-center h-24  flex-col flex items-center border  p-4 dark-sidebar m-4 rounded-xl">
