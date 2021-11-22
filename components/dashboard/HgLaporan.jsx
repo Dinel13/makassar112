57e894632dce5c198b3280d541a89cdace2bcb0a
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-11-22 11:11:39 +0800

    feat : higlight

--- components/dashboard/HgLaporan.jsx
+++ components/dashboard/HgLaporan.jsx
@@ -20,7 +20,7 @@ export default function HgLaporan() {
         <p>11/21/2021</p>
         <p>3:26</p>
       </div>
-      <div className="text-center h-24  flex-col flex items-center border  p-4 dark-sidebar m-4 rounded-xl">
+      {/* <div className="text-center h-24  flex-col flex items-center border  p-4 dark-sidebar m-4 rounded-xl">
         <p>
           fdsfds fsd fas asdasa sadsad sad sad. fasfafasf asdasd asdasd asdasd
           asdasd asd
@@ -35,7 +35,7 @@ export default function HgLaporan() {
         </p>
         <p>1/2y1/2021</p>
         <p>3:26</p>
-      </div>
+      </div> */}
     </Marquee>
   );
 }
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -32,12 +32,12 @@ export default function Dashbord() {
         </div>
       </div>
       <div className="my-8 flex justify-center py-2 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-        <div className="w-full rounded-lg border-2 shadow-xl bg-white dark:bg-dark1 border-gray-600 dark:border-white">
+        {/* <div className="w-full rounded-lg border-2 shadow-xl bg-white dark:bg-dark1 border-gray-600 dark:border-white"> */}
           <h3 className="text-subtitle text-center font-medium mt-5">
             Hightlight laporan
           </h3>
           <HgLaporan />
-        </div>
+        {/* </div> */}
       </div>
       <LaporanTerbaru />
     </div>
