a3fa5bea5ce365f856ad89ee5fb76e9fe3395c3c
Ainun <ayi.jayz13@gmail.com>
2021-11-23 15:20:31 +0800

    updated footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -54,53 +54,9 @@ function Footer() {
               Kontak
             </h2>
             <nav className="list-none mb-10 ">
+              <li>Call Center : 112</li>
               <li>Telepon : (0411) 4671729</li>
               <li>E-mail: diskominfo@makassarkota.go.id</li>
-              <span className="inline-flex sm:ml-auto mt-3 justify-center sm:justify-start">
-                <a
-                  href="https://www.instagram.com/"
-                  rel="noreferrer"
-                  target="_blank"
-                  className=""
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    role="img"
-                    width="1em"
-                    height="1em"
-                    preserveAspectRatio="xMidYMid meet"
-                    viewBox="0 0 24 24"
-                  >
-                    <g fill="currentColor">
-                      <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53a4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-                    </g>
-                  </svg>
-                </a>
-                <a
-                  href="https://www.linkedin.com/"
-                  rel="noreferrer"
-                  target="_blank"
-                  className="ml-3"
-                >
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="0"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke="none"
-                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                    ></path>
-                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                  </svg>
-                </a>
-              </span>
             </nav>
           </div>
         </div>
