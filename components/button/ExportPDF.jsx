6f177ffb2604a4cee2cf5d2271917a31e32e7420
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-11-27 14:34:24 +0800

    feat: expandeble table

--- components/button/ExportPDF.jsx
+++ components/button/ExportPDF.jsx
@@ -4,7 +4,7 @@ export default function ExportPDF({ data }) {
   return (
     <>
       <>
-        <Script src=" https://printjs-4de6.kxcdn.com/print.min.js" />
+        <Script  src=" https://printjs-4de6.kxcdn.com/print.min.js" />
         <Script src=" https://printjs-4de6.kxcdn.com/print.min.css" />
       </>
       <button
@@ -13,22 +13,46 @@ export default function ExportPDF({ data }) {
           printJS({
             printable: data,
             header: "Laporan makassar 112",
-            // properties: [
-            //   { field: 'name', displayName: 'Full Name'},
             properties: [
-              "id",
-              "kategori",
-              "deskripsi",
-              "alamat",
-              "lokasi",
-              "update_at",
-              "status",
+              {field: "id", displayName : "Id"},
+              {field: "id_laporan", displayName : "Id Laporan"},
+              {field: "kategori", displayName : "Kategori"},
+              {field: "deskripsi", displayName : "Deskripsi"},
+              {field: "lokasi", displayName : "Lokasi Kejadian"},
+              {field: "kecamatan", displayName : "Kecamatan"},
+              {field: "kelurahan", displayName : "Kelurahan"},
+              {field: "catatan", displayName : "Catatan"},
+              {field: "created_at", displayName : "Waktu Lapor"},
+              {field: "status", displayName : "Status"},
+              {field: "updated_at", displayName : "Status Time"},
+              {field: "nama", displayName : "Nama Pelapor "},
+              {field: "telp", displayName : "No. Telp "},
+              {field: "channel", displayName : "Channel"},
+              {field: "tipe", displayName : "Tipe Panggilan"},
+              {field: "agenl1", displayName : "Agen L1"},
+              {field: "agenl2", displayName : "Agen L2"},
+              {field: "agenl3", displayName : "Agen L3"},
+              {field: "lat", displayName : "Lattitude"},
+              {field: "long", displayName : "Langitude"},
+              {field: "sub1", displayName : "Subcategori 1"},
+              {field: "sub2", displayName : "Subcategori 2"},
+              {field: "sub3", displayName : "Subcategori 3"},
+              {field: "dinas", displayName : "Dinas Terkait"},
+              {field: "catatanl2", displayName : "Catatan L2"},
+              {field: "catatanl3", displayName : "Catatan L3"},
             ],
             type: "json",
             gridHeaderStyle:
               "background-color: gray;  border: 2px solid #3971A5;",
-            gridStyle: "border: 2px solid #3971A5;",
-            font_size: "10",
+            gridStyle: "border: 1px solid #3971A5;",
+            font_size: "10pt",
+            style : "table-layout: fixed;",
+            header: true,
+            footer: true,
+            width: "100%",
+            height: "100%",
+            css: "",
+            page_orientation: "landscape",
           })
         }
         className="btn-pri py-1.5 text-sm px-5 tracking-wider"
--- components/dashboard/ExpandebleTable.jsx
+++ components/dashboard/ExpandebleTable.jsx
@@ -1,33 +1,100 @@
 import React from "react";
+import { parseDateSQLtoString } from "../../lib/time";
 
 export default function ExpandebleTable({ data }) {
   return (
     <table className="table-fixed mt-2 mb-4 mx-4 text-xs border-b">
       <tbody className="divide-y divide-gray-200">
-        <tr className="w-auto">
-          <td className="w-36 font-medium">No. Telphone</td>
-          <td className="p-2">{data.telp}</td>
+        <tr>
+          <td className="w-36 font-medium">Catatan</td>
+          <td className="p-2">{data.catatan}</td>
         </tr>
         <tr>
-          <td className="w-36 font-medium">Catatan </td>
-          <td className="p-2">{data.catatan}</td>
+          <td className="w-36 font-medium">Waktu Lapor</td>
+          <td className="p-2">{parseDateSQLtoString(data.created_at)}</td>
         </tr>
         <tr>
           <td className="w-36 font-medium">Status</td>
           <td className="p-2">{data.status}</td>
         </tr>
         <tr>
-          <td className="w-36 font-medium">Tipe</td>
+          <td className="w-36 font-medium">Status Time</td>
+          <td className="p-2">{parseDateSQLtoString(data.updated_at)}</td>
+        </tr>
+        <tr>
+          <td className="w-36 font-medium">No. Telp</td>
+          <td className="p-2">{data.telp}</td>
+        </tr>
+        <tr>
+          <td className="w-36 font-medium">Channel</td>
+          <td className="p-2">{data.channel}</td>
+        </tr>
+        <tr>
+          <td className="w-36 font-medium">Tipe Panggilan</td>
           <td className="p-2">{data.tipe}</td>
         </tr>
         <tr>
           <td className="w-36 font-medium">Agen L1</td>
-          <td className="p-2">{data.agen}</td>
+          <td className="p-2">{data.agen1}</td>
         </tr>
+        {data.agen2 !== "-" && (
+          <tr>
+            <td className="w-36 font-medium">Agen L2</td>
+            <td className="p-2">{data.agen2}</td>
+          </tr>
+        )}
+        {data.agen3 !== "-" && (
+          <tr>
+            <td className="w-36 font-medium">Agen L3</td>
+            <td className="p-2">{data.agen3}</td>
+          </tr>
+        )}
+        {data.lat !== "0" && (
+          <tr>
+            <td className="w-36 font-medium">Lat</td>
+            <td className="p-2">{data.lat}</td>
+          </tr>
+        )}
+        {data.long !== "0" && (
+          <tr>
+            <td className="w-36 font-medium">Long</td>
+            <td className="p-2">{data.long}</td>
+          </tr>
+        )}
+        {data.sub1 !== "-" && (
+          <tr>
+            <td className="w-36 font-medium">SubCategory 1</td>
+            <td className="p-2">{data.sub1}</td>
+          </tr>
+        )}
+        {data.sub2 !== "-" && (
+          <tr>
+            <td className="w-36 font-medium">SubCategory 2</td>
+            <td className="p-2">{data.sub2}</td>
+          </tr>
+        )}
+        {data.sub3 !== "-" && (
+          <tr>
+            <td className="w-36 font-medium">SubCategory 3</td>
+            <td className="p-2">{data.sub3}</td>
+          </tr>
+        )}
         <tr>
           <td className="w-36 font-medium">Dinas terkait</td>
           <td className="p-2">{data.dinas}</td>
         </tr>
+        {data.catatanl2 && (
+          <tr>
+            <td className="w-36 font-medium">Catatan L2</td>
+            <td className="p-2">{data.catatanl2}</td>
+          </tr>
+        )}
+        {data.catatanl3 && (
+          <tr>
+            <td className="w-36 font-medium">Catatan L3</td>
+            <td className="p-2">{data.catatanl3}</td>
+          </tr>
+        )}
       </tbody>
     </table>
   );
